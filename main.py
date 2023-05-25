a7925189d16134dd2bca29536c81ca9452f8b48f
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-25 20:29:45 +0300

    4 завдання

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-"""import urllib.request
-opener = urllib.request.build_opener()
-response = opener.open("https://httpbin.org/get")
-print(response.read())"""
-
 import  requests
-response = requests.get('https://coinmarketcap.com/')
-"""print(response.text)
-response_parse =  response.text.split('<span>')
-for elem in response_parse:
-    if elem.startswith("$"):
-        print(elem)"""
 from bs4 import BeautifulSoup
+response = requests.get('https://www.example.com/')
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, features= "html.parser")
-    soup_list = soup.find_all('a', {'href': "/currencies/bitcoin/markets/"})
-    res = soup_list[0].find('span')
-    print(res.text)
+    for script in soup.find_all(["style", "script"]):
+        script.extract()#вирізає HTML приколи
+    text = ' '.join(soup.stripped_strings)#відрізає зайве HTML приколи
+    words = len(text.split())
+    print(words)
